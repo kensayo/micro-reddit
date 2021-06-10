e4d6e25a04434c4e369da22780ca6910be63a5bd
Kenny Ortega <kennyaoc@gmail.com>
2021-06-09 22:19:35 -0400

    Created comments model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,23 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :integer          not null, primary key
+#  title      :string           not null
+#  link       :string           not null
+#  user_id    :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Post < ApplicationRecord
 
-  # Validations
- 
-
   belongs_to :user
+  has_many :comments
+  # Validations
+  validates :title, length: { in: 8..50 }
+  validates :link, format: {
+    with: %r{(https?://)?([\da-z\.-]+\.[a-z\.]{2,6}|[\d\.]+)([/:?=&#_-]{1}[\da-z\.-]+)*[//?]?},
+    message: 'Invalid URL'
+  }
+
 end
--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,17 +1,19 @@
 # == Schema Information
 #
-# Table name: posts
+# Table name: comments
 #
 #  id         :integer          not null, primary key
-#  title      :string           not null
-#  link       :string           not null
+#  comment    :string           not null
+#  likes      :integer          default(0)
+#  dislikes   :integer          default(0)
+#  post_id    :integer          not null
 #  user_id    :integer          not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
 require "test_helper"
 
-class PostTest < ActiveSupport::TestCase
+class CommentTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
