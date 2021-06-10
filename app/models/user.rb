cc7dde11b6747e342f58788bebd556096a370603
Kenny Ortega <kennyaoc@gmail.com>
2021-06-09 20:10:36 -0400

    Validates username, password and email
    Validates that the data is safe

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,26 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  username   :string           not null
+#  email      :string           not null
+#  pwd        :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class User < ApplicationRecord
 
-  #Validates
+  # Validates
   validates :username, presence: true, uniqueness: true
-  validates :pwd, presence: true, uniqueness: true
-  validates :email, presence: true, uniqueness: true
-  validate :check_email
+  validates :pwd, presence: true, length: { in: 8..12 }, format: {
+    with: /(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*\W)/,
+    message: 'Password must be safe'
+  }
+  validates :email, presence: true, uniqueness: true, format: {
+    with: /[a-z0-9]+[_a-z0-9\.-]*[a-z0-9]+@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})/,
+    message: 'Invalid email, please check'
+  }
 
-  private
-  def check_email
-    if self.email =~ /^([\w\.\-_]+)?\w+@[\w-_]+(\.\w+){1,}$/
-      self.errors.add(:email, 'Invalid email, please check')
-    end
-  end
+
 end
